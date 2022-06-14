23a47ae98ceaa7e572bff5426a490bf594268db5
FrasNym <frastyawan.nym@gmail.com>
2022-06-14 10:32:39 +0800

    feat(header): Add links to header

--- components/layouts/ContentLayout/ContentLayout.tsx
+++ components/layouts/ContentLayout/ContentLayout.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import { KeyboardSearch } from "../../pages/search/KeyboardSearch";
 import { Footer } from "../Footer";
+import { Header } from "../Header";
 
 interface Props {
   children?: React.ReactNode;
@@ -9,9 +9,7 @@ interface Props {
 const ContentLayout: NextPage<Props> = ({ children }) => {
   return (
     <main className="bg-white dark:bg-gray-900">
-      <header className="sticky top-0 z-40 flex justify-center flex-none w-full py-3 mx-auto bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800">
-        <KeyboardSearch urlMap="/keyboard/{slug}" />
-      </header>
+      <Header />
       <div className="container px-2 pt-10 pb-5 mx-auto xl:w-6/12">
         <div className="w-full h-full p-2 bg-white border border-gray-200 code-preview rounded-xl bg-gradient-to-r dark:border-gray-700 sm:p-6 dark:bg-gray-800">
           {children}
---
+++ pages/report.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Report: NextPage = () => {
+  return <p>Report Page</p>;
+};
+
+export default Report;
