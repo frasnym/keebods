726990cfa44481d7f80eb2e055ed94c5a539e37f
FrasNym <frastyawan.nym@gmail.com>
2022-06-14 11:10:54 +0800

    feat(home): Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from "next";
+import { Footer } from "../components/layouts/Footer";
 import { KeyboardSearch } from "../components/pages/search/KeyboardSearch";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-white dark:bg-gray-900">
-      <div className="flex items-center justify-center h-screen">
-        <KeyboardSearch urlMap="/keyboard/{slug}" />
+      <div className="flex flex-col items-center h-screen">
+        <div className="flex flex-col items-center justify-center grow">
+          <h1 className="font-extrabold text-7xl sm:text-9xl dark:text-white">
+            Keebods
+          </h1>
+          <h2 className="pt-2 pb-5 font-bold dark:text-white">
+            Mechanical Keyboard Database
+          </h2>
+          <KeyboardSearch urlMap="/keyboard/{slug}" />
+        </div>
+        <div className="w-full">
+          <Footer />
+        </div>
       </div>
     </div>
   );
